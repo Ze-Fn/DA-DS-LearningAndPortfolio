15daad3ff13fb7b67bcd3ced0d8e5c86719681c7
Zelvy Fauzan <129514168+Ze-Fn@users.noreply.github.com>
2025-04-21 06:51:52 +0700

    Update I3. String Statement.sql

--- I3. String Statement.sql
+++ I3. String Statement.sql
@@ -5,10 +5,11 @@ USE parks_and_recrearions;
 SELECT first_name, last_name, CONCAT(LEFT(first_name,1), LEFT(last_name,1)) AS Initials
 FROM employee_demographics;
 
-SELECT birth, LEFT(birth,4) AS Year, SUBSTRING(birth,6,2) AS Month, RIGHT(birth,2) AS Day
+SELECT birth_date, LEFT(birth_date,4) AS Year, SUBSTRING(birth_date,6,2) AS Month, RIGHT(birth_date,2) AS Day
 FROM employee_demographics
 ORDER BY first_name DESC;
 
-/* Coding in progress */
+
+/* Strings statements are pretty much similar to the strings statements found in Excel. I think I can get used to it without any problem */
 
 
