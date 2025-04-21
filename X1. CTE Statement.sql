ce0188a7550eb6869630ddaa0eadd9d9ae1e88a4
Zelvy Fauzan <129514168+Ze-Fn@users.noreply.github.com>
2025-04-21 18:52:02 +0700

    Update X1. CTE Statement.sql

--- X1. CTE Statement.sql
+++ X1. CTE Statement.sql
@@ -15,7 +15,7 @@ WITH CTE_Example AS
     birth_date, 
     employee_id
     FROM employee_demographics as edemo
-    -- WHERE birth_date > '1985-01-01'
+    WHERE birth_date > '1985-01-01'
     ),
 CTE_Example2 AS
 	(SELECT first_name, 
@@ -23,10 +23,34 @@ CTE_Example2 AS
     salary, 
     employee_id
     FROM employee_salary AS esal
-    -- WHERE salary > 25000
+    WHERE salary > 25000
     )
 SELECT *
 FROM CTE_Example
 JOIN CTE_Example2
 	ON CTE_Example.employee_id = CTE_Example2.employee_id
+;
+
+/* Second Exercise */
+WITH FullName_edemo AS
+	(SELECT first_name, last_name
+    FROM employee_demographics
+    ),
+FullName_esal AS
+    (SELECT first_name, last_name
+    FROM employee_salary
+	),
+salary AS
+	(SELECT salary
+    FROM employee_salary
+    ),
+gender AS
+	(SELECT gender
+    FROM employee_demographics
+    )
+SELECT *
+FROM FullName_edemo
+UNION
+SELECT *
+FROM FullName_esal
 ;
