caa08dd368dbbc7ae5073f652d9b87b78420e28b
Zelvy Fauzan <129514168+Ze-Fn@users.noreply.github.com>
2025-04-23 14:19:06 +0700

    Create Temporary Tables.sql

---
+++ Temporary Tables.sql
@@ -0,0 +1,11 @@
+-- Temporary Tables
+
+CREATE TEMPORARY TABLE temp_table
+(first_name varchar (50),
+last_name varchar(50),
+favorite_food varchar (100)
+);
+SELECT *
+FROM temp_table;
+
+CREATE TEMPORARY TABLE
